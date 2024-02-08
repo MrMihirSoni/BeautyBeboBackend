9c23b9c8d9bcad21167883480ee6e0b0e1502613
Mihir Soni <ms6711534@gmail.com>
2024-02-08 15:32:37 +0530

    Minor bug changes

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,24 +1,23 @@
 const ProductModel = require("../models/product.model");
 
 const getAllProducts = async (req, res) => {
-  let { limit } = req.query;
-  const query = req.query;
+  let { category, limit } = req.query;
+  // const query = req.query;
   
   if (limit && !isNaN(limit)) {
     limit = parseInt(limit);
   } else {
     limit = 0; // Default value if limit is not provided or not a valid number
   }
-  
+  console.log(limit)
   try {
     let data;
     if (limit > 0) {
-      data = await ProductModel.find(query).limit(limit);
+      data = await ProductModel.find(category).limit(limit);
     } else {
-      data = await ProductModel.find(query);
+      data = await ProductModel.find(category);
     }
 
-    if (!data) throw new Error("Something went wrong while fetching data!");
     res.status(200).json({ data });
   } catch (error) {
     res.status(400).json({ error: error.message });
