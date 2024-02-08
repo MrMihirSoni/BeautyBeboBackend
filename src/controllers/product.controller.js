3de360917a6a27fd6642ea221e1c3c03ddba78a5
Mihir Soni <ms6711534@gmail.com>
2024-02-08 14:28:26 +0530

    add limit parameter throw query

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,25 @@
 const ProductModel = require("../models/product.model");
 
 const getAllProducts = async (req, res) => {
+  let { limit } = req.query;
   const query = req.query;
+  
+  if (limit && !isNaN(limit)) {
+    limit = parseInt(limit);
+  } else {
+    limit = 0; // Default value if limit is not provided or not a valid number
+  }
+  
   try {
-    const data = await ProductModel.find(query);
-    if (!data) throw new Error("Somethin went wrong while fetching data!");
-    res.status(200).json({ data: data });
+    let data;
+    if (limit > 0) {
+      data = await ProductModel.find(query).limit(limit);
+    } else {
+      data = await ProductModel.find(query);
+    }
+
+    if (!data) throw new Error("Something went wrong while fetching data!");
+    res.status(200).json({ data });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
